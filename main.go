0531a0a0bb9053ce6a1dd0549f9a4bfac3d3569d
faizmohammad <faizmohammad@microsoft.com>
2024-12-21 20:38:47 +0530

    pinging domain

--- main.go
+++ main.go
@@ -1,10 +1,14 @@
 package main
 
-import "github.com/go-ping/ping"
+import (
+	"fmt"
+
+	"github.com/go-ping/ping"
+)
 
 func main() {
 	println("Hello, I am a custom port scanner!")
-	pinger, err := ping.NewPinger("www.google.com")
+	pinger, err := ping.NewPinger("example.com")
 	if err != nil {
 		panic(err)
 	}
@@ -12,5 +16,7 @@ func main() {
 	pinger.Count = 3
 	pinger.Run() // blocks until finished
 	stats := pinger.Statistics()
-	println(stats)
+	fmt.Printf("Ping packets sent: %d\n", stats.PacketsSent)
+	fmt.Printf("Ping packets received: %d\n", stats.PacketsRecv)
+	fmt.Printf("Ping packets lost: %d\n", stats.PacketsSent-stats.PacketsRecv)
 }
